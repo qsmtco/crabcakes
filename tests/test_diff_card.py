72ad70babc2a0a0e08369cd80877dc14f25ddee4
Lieutenant Qrusher <q@snowymountaintattoo.com>
2026-07-15 10:18:07 -0700

    Accept: tests/test_diff_card.py

--- tests/test_diff_card.py
+++ tests/test_diff_card.py
@@ -5,6 +5,8 @@ import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
+import pathlib
+
 from utils.diff_parser import FileDiff, ParsedDiff, DiffHunk, DiffLine
 from ui.views.diff_card import (
     get_lang_from_path,
